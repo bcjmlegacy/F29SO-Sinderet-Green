627e41d87ee4fea219f3332d4f48ac0dfad77d4a
Euan Gordon <48190058+cheerypal@users.noreply.github.com>
2020-01-16 11:14:33 +0000

    Update summary.vue

--- uplink-project-vue/src/components/summary.vue
+++ uplink-project-vue/src/components/summary.vue
@@ -4,11 +4,39 @@
       <div>
         <h1 class="summary-title">Home Usage Today</h1>
       </div>
+      <div>
+        <h5>
+          Energy Usage
+          <span>
+            <img src="../assets/flash.png" class="summary-img" />
+          </span>
+        </h5>
+      </div>
+      <div>
+        <h5>
+          Solar Stored
+          <span>
+            <img src="../assets/battery.png" class="summary-img" />
+          </span>
+        </h5>
+      </div>
+      <div>
+        <h5>
+          Home Temperature
+          <span>
+            <img src="../assets/sun.png" class="summary-img" />
+          </span>
+        </h5>
+      </div>
     </b-col>
   </b-row>
 </template>
 <style>
 .summary-title {
   font-size: 2em;
+}
+
+.summary-img {
+  width: 5% !important;
 }
 </style>
