6917af15415e6b674444dc37f4fe335a6bf64131
Euan Gordon <48190058+cheerypal@users.noreply.github.com>
2020-02-06 12:54:00 +0000

    New Routing System started

--- uplink-project-vue/src/components/roomTemplate.vue
+++ uplink-project-vue/src/components/roomTemplate.vue
@@ -2,16 +2,18 @@
   <!--Room Card-->
   <div class="item">
     <!--Switch to the room page and also set the current room to the roomName said on the card clicked-->
-    <div class="card custom-cards-rooms" @click="switchComp('Room'); updateRoom(roomName)">
-      <div class="img-cont">
-        <!--Image of the room from prop roomImage-->
-        <img class="card-img-top" :src="require(`../assets/${roomImage}.png`)" alt="Energy Usage" />
+    <router-link to="/room" @click="updateRoom(roomName)">
+      <div class="card custom-cards-rooms">
+        <div class="img-cont">
+          <!--Image of the room from prop roomImage-->
+          <img class="card-img-top" :src="require(`../assets/${roomImage}.png`)" alt="Energy Usage" />
+        </div>
+        <!--Room name from prop roomName-->
+        <div class="card-body">
+          <h5 class="card-title text-center">{{roomName}}</h5>
+        </div>
       </div>
-      <!--Room name from prop roomName-->
-      <div class="card-body">
-        <h5 class="card-title text-center">{{roomName}}</h5>
-      </div>
-    </div>
+    </router-link>
   </div>
 </template>
 <script>
--- uplink-project-vue/src/main.js
+++ uplink-project-vue/src/main.js
@@ -1,11 +1,21 @@
 //dependencies.
-
 import Vue from "vue";
 import App from "./App.vue";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "../public/style.css"; //css file
+import VueRouter from "vue-router";
+import Dash from "./components/dashboardTemplate";
+import Room from "./components/roomPageTemplate";
+import AddItem from "./components/addPage";
+import AddRoom from "./components/addRoom";
+import AddDevices from "./components/addDevices";
+import Login from "./components/loginTemplate";
+//import Register from "./components/registerTemplate";
+import AddDeviceMetrics from "./components/addDeviceMetrics";
+import EditDevice from "./components/editDevicePage";
+import deviceDetails from "./components/deviceDetails";
 
 //bootstrap vue tags can be used
 Vue.use(BootstrapVue);
@@ -23,7 +33,58 @@ Vue.$cookies.set("hover-time", "1s");
 //data controller variable.
 export const bus = new Vue();
 
+Vue.use(VueRouter);
+const router = new VueRouter({
+	mode: "history",
+	routes: [
+		{
+			name: "dashboard",
+			path: "/",
+			component: Dash,
+			props: true
+		},
+		{
+			name: "login",
+			path: "/login",
+			component: Login
+		},
+		{
+			name: "room",
+			path: "/room",
+			component: Room,
+			props: true,
+			children: [
+				{
+					path: "deviceDetails",
+					component: deviceDetails,
+					children: [
+						{ path: "editDevice", component: EditDevice, props: true }
+					],
+					props: true
+				}
+			]
+		},
+		{
+			name: "add",
+			path: "/add",
+			component: AddItem,
+			props: true,
+			children: [
+				{ path: "room", component: AddRoom, props: true },
+				{
+					path: "device",
+					component: AddDevices,
+					children: [
+						{ path: "AddData", component: AddDeviceMetrics, props: true }
+					]
+				}
+			]
+		}
+	]
+});
+
 //mounts app to the html page - public/index.html
 new Vue({
+	router,
 	render: h => h(App)
 }).$mount("#app");
