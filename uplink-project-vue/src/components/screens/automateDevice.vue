63461c950b90a54d907ee047be49ba68be58c15b
Euan Gordon <48190058+cheerypal@users.noreply.github.com>
2020-03-08 16:41:05 +0000

    Responsiveness Tweaks

--- uplink-project-vue/src/components/screens/automateDevice.vue
+++ uplink-project-vue/src/components/screens/automateDevice.vue
@@ -8,6 +8,28 @@
         </h5>
       </div>
     </div>
+    <div id="automate">
+      <div class="flex-deviceDetails">
+        <div class="custom-cards-editDevices">
+          <div class="img-cont">
+            <img
+              class="device-img"
+              :src="require(`../../assets/${deviceImage}.png`)"
+              alt="device icon"
+            />
+          </div>
+          <div class="text-wrapper">
+            <h5 class="card-title text-center label-section">{{deviceName}}</h5>
+            <p class="card-text text-center">{{deviceEnergy}} Watts</p>
+          </div>
+          <div class="device-cont">
+            <b-form>
+              <p class="label-section text-center">Add Automated Events</p>
+            </b-form>
+          </div>
+        </div>
+      </div>
+    </div>
     <NavbarBottom class="bottom-show" />
   </div>
 </template>
@@ -34,4 +56,8 @@ export default {
 </script>
 
 <style>
+#automate {
+  margin-top: 120px;
+  margin-bottom: 80px;
+}
 </style>
